38742e7471fe187c33d5037affac926cfe13def6
StephanieMarques <stephanie.msantos9@senac.edu.br>
2024-06-11 20:25:25 -0300

    Operadores Loógicos

--- 00/script.js
+++ 00/script.js
@@ -23,12 +23,41 @@ class Conta {
     }
 }
 
+class ContaCorrente extends Conta {
+    constructor(numero, saldo = 0, limiteChequeEspecial = 200) {
+        super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
 
-conta1 = new Conta (1001, 50);
-conta2 = new Conta (1002);
+    sacar(valor) {
+        //super.sacar(valor);
+        const valorDisponivelSaque = this.saldo + this.limiteChequeEspecial;
 
-conta1.depositar (30);
-conta2.depositar (30);
+        if(valor > 0 && valorDisponivelSaque >= valor) {
+            this.saldo -= valor;
+        } else {
+            console.log("Saldo insuficiente");
+        }
+    }
+}
 
-console.log(conta1.toString());
-console.log(conta2.toString());
+class ContaPoupanca extends Conta {    
+    constructor(numero, saldo = 0, taxaRendimento = 0.2) {
+        super(numero, saldo)
+        this.taxaRendimento = taxaRendimento;
+    }
+
+    aplicarRendimento() {
+        console.log("Rendimento aplicado");
+    }
+}
+
+let contaC = new ContaCorrente(2001, 500);
+let contaP = new ContaPoupanca(2002, 500);
+
+console.log(contaC.toString());
+contaC.sacar(650);
+console.log(contaC.toString());
+
+contaP.aplicarRendimento();
+//contaC.aplicarRendimento(); # erro por não existir função nessa classe
